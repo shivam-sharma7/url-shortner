f98c59c14769b1ef75c5e0fd298d7c83e7f5799b
Shivam Sharma <meshivam81@gmail.com>
2024-04-05 08:27:02 +0530

    configure tailwind and components

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+ 
+
+const Footer = () => {
+  return (
+    <div className=" flex-col bg-slate-900 text-white text-base text-center py-5">
+      Copyright &#169; URLShortner | Shivam Sharma
+    </div>
+  )
+}
+
+export default Footer
---
+++ client/src/components/Header.tsx
@@ -0,0 +1,16 @@
+
+
+const Header = () => {
+  return (
+    <div className="bg-slate-900"> 
+    <div className="w-full p-2 mx-auto">
+      <nav className="py-4">
+        <div className="text-base text-white">URLShortner</div>
+      </nav>
+    </div>
+    </div>
+  
+  )
+}
+
+export default Header
